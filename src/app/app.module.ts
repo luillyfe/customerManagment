0d043dd313a9d12732cc264865c147476d98deed
Fermin Blanco <fermin.blanco@cdev.global>
2018-08-23 15:38:34 -0600

    Customer bones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'customer', component: CustomerComponent },
+      { path: '', redirectTo: 'customer', pathMatch: 'full' },
+      { path: '**', redirectTo: 'customer', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
